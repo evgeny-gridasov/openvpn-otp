45dbdc8ee328420f57439d009eb2f1bed969f04f
Rostan KEZAL <seemyping@outlook.fr>
2018-08-10 12:25:10 +0200

    Updated autogen.sh to compile auto

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-mkdir m4
+
+if [ USER != 'root']; then
+	echo 'Script must be run as root'
+	exit 1
+fi
+
+if [ ! -d "m4" ]; then
+	mkdir m4
+fi
+
+if [ uname == 'FreeBSD' ]; then
+	pkg install devel/autoconf devel/libtool devel/openssl-devel
+elif [ uname | grep 'Linux debian' !=  '' ]; then
+	apt install -y autoconf libtool openssl-devel
+else 
+	echo 'OS not supported, compile it manually'
+	exit 1
+fi
+
 autoreconf -ivf
+./configure --prefix=/usr
+make install
+
+
